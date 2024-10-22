9533c1fa4bf78027957f9096d001c731aab3d2ac
abdellaheltrach <abdellah.eltrach@gmail.com>
2024-10-22 13:20:08 +0100

    add the three-tier architecture layers and the main classes persons and countries

--- DVLD/Form1.Designer.cs
+++ DVLD/Form1.Designer.cs
@@ -34,9 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1094, 586);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -44,4 +45,3 @@
         #endregion
     }
 }
-
---
+++ DVLD_BusinessLayer/clsCountry.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DVLD_DataAccessLayer;
+
+namespace DVLD_BusinessLayer
+{
+    public class clsCountry
+    {
+        public int CountryID { get; set; }
+        public string CountryName { get; set; }
+
+        private clsCountry(int CountryID, string CountryName) 
+        { 
+            this.CountryID = CountryID;
+            this.CountryName = CountryName;
+        }
+
+        public clsCountry(string CountryName)
+        {
+            this.CountryID = FindByName(CountryName).CountryID;
+            this.CountryName = CountryName;
+        }
+
+        public clsCountry(int CountryID)
+        {
+            this.CountryID = CountryID;
+            this.CountryName = FindByID(CountryID).CountryName;
+        }
+
+
+        public static clsCountry FindByID(int ID)
+        {
+
+            string CountryName = "";
+            DateTime DateOfBirth = DateTime.Now;
+            int CountryID = -1;
+
+            if (clsCountryDataAccess.GetCountryInfoByID(ID, ref CountryName))
+
+                return new clsCountry(ID, CountryName);
+            else
+                return null;
+
+        }
+
+        public static clsCountry FindByName(string CountryName)
+        {
+
+            int ID = -1;
+
+
+            if (clsCountryDataAccess.GetCountryInfoByName(CountryName, ref ID))
+
+                return new clsCountry(ID, CountryName);
+            else
+                return null;
+
+        }
+        public static DataTable GetAllCountries()
+        {
+            return clsCountryDataAccess.GetAllCountries();
+
+        }
+
+
+    }
+}
